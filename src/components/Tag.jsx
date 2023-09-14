7c5911157b6de6294b68bdd3179f93457e5213c3
Francis <francisseneve@gmail.com>
2023-09-14 21:08:51 +0100

    Added tag functionality

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,8 +1,14 @@
 import './Tag.css';
 
-const Tag = ({name, selectedTag})=>{
+const Tag = ({name, selectedTag, selected})=>{
+    const tagStyle = {
+        HTML: {backgroundColor: '#fda821'},
+        PHP: {backgroundColor: '#15d4c8'},
+        REACT: {backgroundColor: '#ffd12c'},
+        default: {backgroundColor: '#f9f9f9'},
+    };
     return (
-        <button type='button' onClick={()=>selectedTag(name)} className="tag">{name}</button>
+        <button style={selected ? tagStyle[name] : tagStyle.default} type='button' onClick={()=>selectedTag(name)} className="tag">{name}</button>
     )
 }
 
